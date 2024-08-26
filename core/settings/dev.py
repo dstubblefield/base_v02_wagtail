f43b72bedf8b1e5806a9be82907571df97461159
Andrew Stubblefield <drew@dstubblefield.com>
2024-08-26 05:35:45 -0500

    initial commit

---
+++ core/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-*qfkj&x_x+6or-6@j$ki(alc-s8+yg^@^hs4qb0gz+#(@yo339"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
---
+++ location_funnels/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class LocationFunnelsConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "location_funnels"
